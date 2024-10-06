6035a78ee1c546041e1151acdf7aa384a00d29ac
Kane Sweet <kanesweet11@gmail.com>
2024-10-06 12:43:54 -0500

    cleanup

--- app/src/main/java/org/kanesweet/App.java
+++ app/src/main/java/org/kanesweet/App.java
@@ -3,19 +3,12 @@ package org.kanesweet;
 
 
 public class App {
-
-    GrpcServer server = new GrpcServer();
-    GrpcClient client = new GrpcClient();
-
-    public void test() {
-        System.out.println("Starting...");
-    }
-
     public static void main(String[] args) {
         try {
-            App app = new App();
-            app.server.start();
-            app.client.start();
+            GrpcServer server = new GrpcServer();
+            GrpcClient client = new GrpcClient();
+            server.start();
+            client.start();
         } catch (Exception e) {
             System.err.println(e);
         }
